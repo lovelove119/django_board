932e3bf5acd2257d17b48618a5c67ba9cc8353d5
lovelove119 <mujung123@naver.com>
2023-06-15 15:30:19 +0900

    a목요일

--- board_yproject/board_main/views_test.py
+++ board_yproject/board_main/views_test.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
+from .models import Test
 # Create your views here.
 
 # get요청시 html파일 그대로 return
@@ -58,12 +59,63 @@ def test_post_form(request):
 
 def test_post_handle(request):
    if request.method == "POST":
-      name = request.POST['my_name']
-      email = request.POST['my_email']
-      password = request.POST['my_password']
-      print(name)
-      print(email)
-      print(password)
+      my_name = request.POST['my_name']
+      my_email = request.POST['my_email']
+      my_password = request.POST['my_password']
+      # cd insert -> save함수 사용
+      # DB의 테이블과 save가 맞는 Test클래스에서 객체를 만들어 save
+      t1 = Test()
+      t1.name = my_name
+      t1.email = my_email
+      t1.password = my_password
+      t1.save()
       return redirect('/') #localhost:8000/
    else :
-      return render(request, 'test/test_post_form.html')
+      return render(request, 'test/test_post_form.html', {'data':t1})
+
+def test_select_one(request, my_id):
+  # 단건만을 조회할땐 get() 함수
+  t1= Test.objects.get(id=my_id)
+  print(t1.name)
+  print(t1.email)
+  print(t1.password)
+  return render(request, 'test/test_select_one.html')
+   
+def test_select_all(request):
+  # 모든 data조회 : select ' from xxxx; all()함수사용
+  # dicta.keys()
+  # for 문으로 print
+  tests= Test.objects.all()
+  return render(request, 'test/test_select_all.html', {'datas':tests})
+   
+# where조건으로 다건을 조회할떄 filter()함수 사용
+# Test.objects.filrtet(name = my_namya) ->
+# http://127.0.0.1:8000/test_select_filter?name=aaaaa
+def test_select_filter(request):
+  my_name = request.GET.get('name')
+  tests= Test.objects.filter(name=my_name)
+  return render(request, 'test/test_select_filter.html', {'datas':tests})
+
+# update를 하기위해서는 해당건을 사전에 조회하기 위한 Id값이 필요
+# 메서드는 등록과 동일하게 save()함수 사용
+# save함수는 신규객체를 save하면 lnsert, 기준객체를 save하면 update
+
+def test_update(request):
+   if request.method == "POST":
+      my_id = request.POST['my_id']
+      t1= Test.objects.get(id=my_id)
+      my_name = request.POST['my_name']
+      my_email = request.POST['my_email']
+      my_password = request.POST['my_password']
+     
+      t1.name = my_name
+      t1.email = my_email
+      t1.password = my_password
+      t1.save()
+ #DB설정에 default timestmap가 걸리는 것이 아닌 장고가 ㅎ현재시간을 db에 insert
+      # t1 delete()를 하면 삭제
+      return redirect('/') #localhost:8000/
+   else :
+      return render(request, 'test/test_update.html')
+   
+   # 삭제는 delete()함수 사용, updata와 마찬가지로 기존객체 
